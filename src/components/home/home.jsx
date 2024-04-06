327195b7f265a6871eb0cdbcb88a2534bdf77b3a
unknown <alidev1414@gmail.com>
2024-04-06 18:50:28 +0900

    the motor page is success completed

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Boxes, CarContainer, HomeButton, HomeButtonContainer, HomeContainer, HomeLeftContainer, HomeRightContainer, HomeRightTop, Line, LongLine, MappingButton } from '../../style'
+import { CardButtonWrapper, Price, Boxes, BoxesData, BoxesDataInput, BoxesDataWrapper, BoxesDownArrow, BoxesTitleWrapper, CarContainer, Card, CardBottom, CardBottomCarName, CardBottomMiddle, CostBox, CostBoxInput, HomeButton, HomeButtonContainer, HomeContainer, HomeLeftCarWrapper, HomeLeftContainer, HomeLeftParagraph, HomeRightContainer, HomeRightTop, HomeRightTopButton, HomeRightTopLeft, HomeRightTopLeftRight, HomeRightTopRight, Line, LongLine, MappingButton, SortInput, Div, CardImg } from '../../style'
 
 import { campcar } from '../mock/mockdata'
 
@@ -19,187 +19,198 @@ function Home() {
     <HomeContainer>
         <HomeLeftContainer>
             <div>
-                <p style={{fontWeight: "700", fontSize: "18px"}}>Cost of car</p>
+                <HomeLeftParagraph>Cost of car</HomeLeftParagraph>
                 <Line/>
-                <div style={{marginTop: "30px", display: "flex", gap: "25px"}}>
+                <CostBox>
                     <div>
                         <label htmlFor="from">from</label> <br />
-                        <input type="text" style={{border: "1px solid rgba(55, 55, 55, 0.60)", height: "36px", width: "90px", borderRadius: "10px"}} id='from'/>
+                        <CostBoxInput type="text" id='from'/>
                     </div>
                     <div>
                         <label htmlFor="to">to</label> <br />
-                        <input type="text" style={{border: "1px solid rgba(55, 55, 55, 0.60)", height: "36px", width: "90px", borderRadius: "10px"}} id='to'/>
+                        <CostBoxInput type="text" id='to'/>
                     </div>
-                </div>
+                </CostBox>
+
                 <Boxes>
-                    <div style={{display: "flex", justifyContent:"space-between"}}>
+                    <BoxesTitleWrapper>
                         <h3>Brand</h3>
-                        <img src={downArrow} alt="" style={{marginRight: "50px"}} />
-                    </div>
+                        <BoxesDownArrow src={downArrow} />
+                    </BoxesTitleWrapper>
+
                     <Line/>
-                    <div style={{marginTop: "30px", display: "flex", flexDirection: "column", gap: "10px"}}>
-                        <div style={{display: "flex", gap: "10px"}}>
-                            <input type="checkbox" name="" id="aidal" style={{width: "22px", height: "22px"}}/>
+
+                    <BoxesDataWrapper>
+                        <BoxesData style={{display: "flex", gap: "10px"}}>
+                            <BoxesDataInput type="checkbox" id="aidal"/>
                             <label htmlFor="aidal">Aidal</label>
-                        </div>
-                        <div style={{display: "flex", gap: "10px"}}>
-                            <input type="checkbox" name="" id="knal" style={{width: "22px", height: "22px"}}/>
+                        </BoxesData>
+                        <BoxesData style={{display: "flex", gap: "10px"}}>
+                            <BoxesDataInput type="checkbox" id="knal"/>
                             <label htmlFor="knal">Knal</label>
-                        </div>
-                        <div style={{display: "flex", gap: "10px"}}>
-                            <input type="checkbox" name="" id="escape" style={{width: "22px", height: "22px"}}/>
+                        </BoxesData>
+                        <BoxesData style={{display: "flex", gap: "10px"}}>
+                            <BoxesDataInput type="checkbox" id="escape"/>
                             <label htmlFor="escape">escape</label>
-                        </div>
-                    </div>
+                        </BoxesData>
+                    </BoxesDataWrapper>
                 </Boxes>
+
                 <Boxes>
-                    <div style={{display: "flex", justifyContent:"space-between"}}>
+                    <BoxesTitleWrapper>
                         <h3>Company</h3>
-                        <img src={downArrow} alt="" style={{marginRight: "50px"}} />
-                    </div>
+                        <BoxesDownArrow src={downArrow} alt="" />
+                    </BoxesTitleWrapper>
+
                     <Line/>
-                    <div style={{marginTop: "30px", display: "flex", flexDirection: "column", gap: "10px"}}>
-                        <div style={{display: "flex", gap: "10px"}}>
-                            <input type="checkbox" name="" id="escape2" style={{width: "22px", height: "22px"}}/>
+
+                    <BoxesDataWrapper>
+                        <BoxesData>
+                            <BoxesDataInput type="checkbox" id="escape2"/>
                             <label htmlFor="escape2">escape</label>
-                        </div>
-                        <div style={{display: "flex", gap: "10px"}}>
-                            <input type="checkbox" name="" id="aidal2" style={{width: "22px", height: "22px"}}/>
+                        </BoxesData>
+                        <BoxesData>
+                            <BoxesDataInput type="checkbox" id="aidal2"/>
                             <label htmlFor="aidal2">Aidal</label>
-                        </div>
-                    </div>
+                        </BoxesData>
+                    </BoxesDataWrapper>
                 </Boxes>
+
                 <Boxes>
-                    <div style={{display: "flex", justifyContent:"space-between"}}>
+                    <BoxesTitleWrapper>
                         <h3>License type</h3>
-                        <img src={downArrow} alt=""  style={{marginRight: "50px"}}/>
-                    </div>
+                        <BoxesDownArrow src={downArrow} alt=""/>
+                    </BoxesTitleWrapper>
+
                     <Line/>
-                    <div style={{marginTop: "30px", display: "flex", flexDirection: "column", gap: "10px"}}>
-                        <div style={{display: "flex", gap: "10px"}}>
-                            <input type="checkbox" name="" id="1year" style={{width: "22px", height: "22px"}}/>
+
+                    <BoxesDataWrapper>
+                        <BoxesData>
+                            <BoxesDataInput type="checkbox" name="" id="1year"/>
                             <label htmlFor="1year">1 year</label>
-                        </div>
-                        <div style={{display: "flex", gap: "10px"}}>
-                            <input type="checkbox" name="" id="2year" style={{width: "22px", height: "22px"}}/>
+                        </BoxesData>
+                        <BoxesData>
+                            <BoxesDataInput type="checkbox" name="" id="2year"/>
                             <label htmlFor="2year">2 year</label>
-                        </div>
-                        <div style={{display: "flex", gap: "10px"}}>
-                            <input type="checkbox" name="" id="15year" style={{width: "22px", height: "22px"}}/>
+                        </BoxesData>
+                        <BoxesData>
+                            <BoxesDataInput type="checkbox" name="" id="15year"/>
                             <label htmlFor="15year">1.5year</label>
-                        </div>
-                    </div>
+                        </BoxesData>
+                    </BoxesDataWrapper>
                 </Boxes>
+
                 <Boxes>
-                    <div style={{display: "flex", justifyContent:"space-between"}}>
+                    <BoxesTitleWrapper>
                         <h3>Number of travelers</h3>
-                        <img src={downArrow} alt=""  style={{marginRight: "50px"}}/>
-                    </div>
+                        <BoxesDownArrow src={downArrow} alt=""/>
+                    </BoxesTitleWrapper>
                     <Line/>
-                    <div style={{marginTop: "30px", display: "flex", flexDirection: "column", gap: "10px"}}>
-                        <div style={{display: "flex", gap: "10px"}}>
-                            <input type="checkbox" name="" id="two" style={{width: "22px", height: "22px"}}/>
+                    <BoxesDataWrapper>
+                        <BoxesData>
+                            <BoxesDataInput type="checkbox" name="" id="two"/>
                             <label htmlFor="two">2</label>
-                        </div>
-                        <div style={{display: "flex", gap: "10px"}}>
-                            <input type="checkbox" name="" id="three-minus-four" style={{width: "22px", height: "22px"}}/>
+                        </BoxesData>
+                        <BoxesData>
+                            <BoxesDataInput type="checkbox" name="" id="three-minus-four"/>
                             <label htmlFor="three-minus-four">3-4</label>
-                        </div>
-                        <div style={{display: "flex", gap: "10px"}}>
-                            <input type="checkbox" name="" id="five-plus" style={{width: "22px", height: "22px"}}/>
+                        </BoxesData>
+                        <BoxesData>
+                            <BoxesDataInput type="checkbox" name="" id="five-plus"/>
                             <label htmlFor="five-plus">5+</label>
-                        </div>
-                    </div>
+                        </BoxesData>
+                    </BoxesDataWrapper>
                 </Boxes>
                 <Boxes>
-                    <div style={{display: "flex", justifyContent:"space-between"}}>
+                    <BoxesTitleWrapper>
                         <h3>Location</h3>
-                        <img src={downArrow} alt="" style={{marginRight: "50px"}}/>
-                    </div>
+                        <BoxesDownArrow src={downArrow} alt=""/>
+                    </BoxesTitleWrapper>
                     <Line/>
-                    <div style={{marginTop: "30px", display: "flex", flexDirection: "column", gap: "10px"}}>
-                        <div style={{display: "flex", gap: "10px"}}>
-                            <input type="checkbox" name="" id="seoul" style={{width: "22px", height: "22px"}}/>
+                    <BoxesDataWrapper>
+                        <BoxesData>
+                            <BoxesDataInput type="checkbox" name="" id="seoul"/>
                             <label htmlFor="seoul">Seoul</label>
-                        </div>
-                        <div style={{display: "flex", gap: "10px"}}>
-                            <input type="checkbox" name="" id="Korea" style={{width: "22px", height: "22px"}}/>
+                        </BoxesData>
+                        <BoxesData>
+                            <BoxesDataInput type="checkbox" name="" id="Korea"/>
                             <label htmlFor="Korea">Korea</label>
-                        </div>
-                        <div style={{display: "flex", gap: "10px"}}>
-                            <input type="checkbox" name="" id="Pusan" style={{width: "22px", height: "22px"}}/>
+                        </BoxesData>
+                        <BoxesData>
+                            <BoxesDataInput type="checkbox" name="" id="Pusan"/>
                             <label htmlFor="Pusan">Pusan</label>
-                        </div>
-                    </div>
+                        </BoxesData>
+                    </BoxesDataWrapper>
                 </Boxes>
                 <HomeButtonContainer>
                     <HomeButton>Cancel</HomeButton>
                     <HomeButton $search>Search</HomeButton>
                 </HomeButtonContainer>
 
-                <div style={{marginTop: '50px', color: "#006DAB", fontSize: "17px", fontWeight: "700"}}>
+                <HomeLeftCarWrapper>
                     <p>Compare</p>
                     <div>
                         <img src={img1} alt="" />
                         <img src={img2} alt="" />
                         <img src={img3} alt="" />
                     </div>
-                </div>
+                </HomeLeftCarWrapper>
             </div>
         </HomeLeftContainer>
 
         <HomeRightContainer>
             <HomeRightTop>
-                <div className='mobile-flex' style={{display: "flex", alignItems: "center"}}>
-                    <div style={{fontWeight: "600"}}>
-                        <p className='item'>Item  <span style={{marginLeft: "10px", color: "#006DAB"}}>25.156</span></p>
+                <HomeRightTopLeft>
+                    <div>
+                        <p style={{fontWeight: "600"}}>Item<span style={{marginLeft: "10px", color: "#006DAB"}}>{data.length}</span></p>
                     </div>
-                    <div style={{display: "flex", alignItems: "center"}}>
-                        <button style={{height: "40px", border: "1px solid rgba(55, 55, 55, 0.30)", borderRadius: "5px 0 0 5px", background: "none", width: "40px", cursor: "pointer", marginLeft: '20px'}}><img src={fourDots} alt=""/></button>
-                        <button style={{height: "40px", border: "1px solid rgba(55, 55, 55, 0.30)", borderRadius: "0 5px 5px 0", background: "none", width: "40px", cursor: "pointer"}}><img src={menu} alt=""/></button>
-                    </div>
-                </div>
+                    <HomeRightTopLeftRight $displayNone>
+                        <HomeRightTopButton><img src={fourDots} alt=""/></HomeRightTopButton>
+                        <HomeRightTopButton $rightButton><img src={menu} alt=""/></HomeRightTopButton>
+                    </HomeRightTopLeftRight>
+                </HomeRightTopLeft>
                 
-                <div style={{display: "flex", alignItems: "center"}}>
-                    <p style={{fontWeight: "500"}}>Sort by</p>
-                    <div style={{display: "flex", alignItems: "center"}}>
-                        <input type="text" placeholder='Select' style={{height: "40px", border: "1px solid rgba(55, 55, 55, 0.30)", borderRadius: "5px 0 0 5px", padding: "0 10px", marginLeft: "10px"}}/>
-                        <button style={{height: "40px", border: "1px solid rgba(55, 55, 55, 0.30)", borderRadius: "0 5px 5px 0", background: "none", width: "40px", cursor: "pointer"}}><img src={downArrow} alt=""/></button>
+                <HomeRightTopRight>
+                    <p>Sort<span style={{marginLeft: '5px'}}>by</span></p>
+                    <div>
+                        <SortInput type="text" placeholder='Select'/>
+                        <HomeRightTopButton $rightButton><img src={downArrow} alt=""/></HomeRightTopButton>
                     </div>
-                    <div style={{display: "flex", alignItems: "center"}}>
-                        <input type="text" placeholder='60' style={{height: "40px", border: "1px solid rgba(55, 55, 55, 0.30)", borderRadius: "5px 0 0 5px", padding: "0 10px", marginLeft: "20px", width: "60px"}}/>
-                        <button style={{height: "40px", border: "1px solid rgba(55, 55, 55, 0.30)", borderRadius: "0 5px 5px 0", background: "none", width: "40px", cursor: "pointer"}}><img src={downArrow} alt=""/></button>
+                    <div>
+                        <SortInput type="text" placeholder='60' $short/>
+                        <HomeRightTopButton $rightButton><img src={downArrow} alt=""/></HomeRightTopButton>
                     </div>
-                    <div style={{display: "flex", alignItems: "center"}}>
-                        <button style={{height: "40px", border: "1px solid rgba(55, 55, 55, 0.30)", borderRadius: "5px 0 0 5px", background: "none", width: "40px", cursor: "pointer", marginLeft: '20px'}} className='mobile-none'><img src={fourDots} alt=""/></button>
-                        <button style={{height: "40px", border: "1px solid rgba(55, 55, 55, 0.30)", borderRadius: "0 5px 5px 0", background: "none", width: "40px", cursor: "pointer"}} className='mobile-none'><img src={menu} alt=""/></button>
+                    <div className='none'>
+                        <HomeRightTopButton><img src={fourDots} alt=""/></HomeRightTopButton>
+                        <HomeRightTopButton $rightButton><img src={menu} alt=""/></HomeRightTopButton>
                     </div>
-                </div>
+                </HomeRightTopRight>
             </HomeRightTop>
-            
             <LongLine/>
 
             <CarContainer>
                 {data.map((value, key)=> {
                     return (
-                        <div key={key} style={{display: "flex", flexDirection: "column", alignItems: "center", boxShadow: "0px 0px 5px 0px rgba(0, 0, 0, 0.10)", padding: "10px 0"}} className='car-box'>
-                            <img src={value.car.photo} alt="Photo of car not entered" />
-                            <div>
-                                <p style={{fontSize: "19px", fontWeight: "600", marginBottom: "5px"}}>{value.car.name}</p>
-                                <div style={{display: "flex", justifyContent: "space-between", alignItems: "start", width: "100%", marginBottom: "20px"}}>
-                                    <p style={{fontSize: "14px", fontWeight: "400"}}>{value.car.company}</p>
-                                    <div style={{display: "flex", alignItems: "center", height: "20px"}}>
+                        <Card key={key}>
+                            <CardImg>
+                                <img src={value.car.photo} alt="Photo of car not entered" width={"100%"}/>
+                            </CardImg>
+                            <CardBottom>
+                                <CardBottomCarName>{value.car.name}</CardBottomCarName>
+                                <CardBottomMiddle>
+                                    <p>{value.car.company}</p>
+                                    <div>
                                         <img src={star} alt="" width={"20px"}/>
-                                        <p>5.3</p>
+                                        <label>5.3</label>
                                     </div>
-                                </div>
-                                <p style={{color: "#006DAB", fontSize: "20px", fontWeight: "700"}}>{value.car.cost} Won</p>
-                                <div style={{display: "flex", gap: '20px'}}>
+                                </CardBottomMiddle>
+                                <Price>{value.car.cost} Won</Price>
+                                <CardButtonWrapper>
                                     <MappingButton>Order</MappingButton>
                                     <MappingButton>Compare</MappingButton>
-                                </div>
-                            </div>
-                        </div>
+                                </CardButtonWrapper>
+                            </CardBottom>
+                        </Card>
                     )
                 })}
             </CarContainer>
--- src/style.js
+++ src/style.js
@@ -32,6 +32,7 @@ export const NavbarContainer = styled.div`
         .mobile-menu {
             display: flex;
         }
+        padding: 20px;
     }
 `
 
@@ -79,15 +80,66 @@ export const HomeContainer = styled.div`
     gap: 30px;
     justify-content: center;
     margin-top: 60px;
+    padding: 0 20%;
 `
 
 export const HomeLeftContainer = styled.div`
     display: flex;
     flex-direction: column;
     gap: 50px;
-    @media only screen and (max-width: 1300px) {
+    flex: 1;
+    @media only screen and (max-width: 1407px) {
         display: none;
     }
+`
+
+export const HomeLeftParagraph = styled.p`
+    font-weight: 600;
+    font-size: 18px;
+`
+
+export const CostBox = styled.div`
+    margin-top: 30px;
+    display: flex;
+    gap: 25px;
+`
+export const CostBoxInput = styled.input`
+    border: 1px solid rgba(55, 55, 55, 0.60);
+    height: 36px; 
+    width: 90px; 
+    border-radius: 10px;
+`
+
+export const BoxesTitleWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+
+export const BoxesDownArrow = styled.img`
+    margin-right: 50px;
+`
+
+export const BoxesDataWrapper = styled.div`
+    margin-top: 30px;
+    display: flex;
+    flex-direction: column; 
+    gap: 10px;
+`
+
+export const BoxesData = styled.div`
+    display: flex;
+    gap: 10px;
+`
+
+export const BoxesDataInput = styled.input`
+    width: 22px;
+    height: 22px;
+`
+export const HomeLeftCarWrapper = styled.div`
+    margin-top: 50px;
+    color: #006DAB;
+    font-size: 17px;
+    font-weight: 700;
 `
 
 export const Line = styled.div`
@@ -125,6 +177,7 @@ export const HomeButton = styled.button`
 export const HomeRightContainer = styled.div`
     display: flex;
     flex-direction: column;
+    flex: 8;
     @media only screen and (max-width: 1300px) {
         grid-template-columns: repeat(2, 1fr);
     }
@@ -133,8 +186,6 @@ export const HomeRightContainer = styled.div`
 export const HomeRightTop = styled.div`
     display: flex;
     justify-content: space-between;
-    /* width: 1000px; */
-    width: 100%;
     align-items: center;
     @media only screen and (max-width: 1300px) {
        display: flex;
@@ -143,31 +194,128 @@ export const HomeRightTop = styled.div`
        width: 100%;
     }
     @media only screen and (max-width: 660px) {
-        .mobile-flex {
-            display: flex;
-            gap: 210px;
-            justify-content: center;
-            width: 75%;
-        }
-        .item {
-            display: flex;
-        }
         flex-direction: column;
         gap: 20px;
-        .mobile-none {
+        .none {
             display: none;
         }
     }
 `
 
+export const HomeRightTopLeft = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+export const HomeRightTopLeftRight = styled.div`
+    display: ${(props) => props.$displayNone ? "none" : "flex"};
+    align-items: center;
+    margin-left: 20px;
+    @media only screen and (max-width: 660px) {
+        display: ${(props) => props.$displayNone ? "flex" : "none"};
+    }
+`
+export const HomeRightTopButton = styled.button`
+    height: 40px; 
+    border: 1px solid rgba(55, 55, 55, 0.30);
+    border-radius: ${(props) => props.$rightButton ? "0 5px 5px 0": "5px 0 0 5px"};
+    background: none;
+    width: 40px;
+    cursor: pointer;
+`
+
+export const HomeRightTopRight = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    div {
+        display: flex;
+        align-items: center;
+    }
+    @media only screen and (max-width: 1089px) {
+        display: flex;
+    }
+    @media only screen and (max-width: 451px) {
+        gap: 0;
+    }
+`
+
+export const SortInput = styled.input`
+    height: 40px; 
+    border: 1px solid rgba(55, 55, 55, 0.30); 
+    border-radius: 5px 0 0 5px;
+    padding: 0 10px;
+    margin-left: 10px;
+    width: ${(props) => props.$short ? "60px" : ""};
+`
+
+export const Card = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.10);
+    padding: 10px 30px;
+    border-radius: 20px;
+    width: 100%;
+    @media only screen and (max-width: 420px) {
+        width: 80%;
+        margin: auto;
+    }
+   
+`
+
+export const CardBottom = styled.div`
+    flex: 1;
+`
+
+export const CardImg = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+`
+
+export const CardBottomCarName = styled.p`
+    font-size: 19px;
+    font-weight: 600;
+    margin-bottom: 5px;
+`
+
+export const CardBottomMiddle = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    margin-bottom: 20px;
+    p {
+        font-size: 14px;
+        font-weight: 400;
+    }
+    div {
+        display: flex;
+        align-items: center;
+        height: 20px;
+    }
+`
+
+export const CardButtonWrapper = styled.div`
+    display: flex;
+    gap: 20px;
+`
+
+export const Price = styled.p`
+    color: #006DAB;
+    font-size: 20px;
+    font-weight: 700;
+`
+
 export const LongLine = styled.div`
-    /* width: 1000px; */
     width: 100%;
     margin-top: 10px;
     height: 1px;
     background: rgba(55, 55, 55, 0.50);
     @media only screen and (max-width: 1300px) {
-        width: 300px;
+        /* width: 300px; */
+        width: 100%;
         display: flex;
         margin: 10px auto;
     }
@@ -181,19 +329,13 @@ export const CarContainer = styled.div`
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     gap: 20px;
-    img {
-        height: 150px;
-    }
     .car-box {
         width: 90%;
     }
     @media only screen and (max-width: 1300px) {
         grid-template-columns: repeat(2, 1fr);
-        /* width: 0; */
         display: grid;
-        /* justify-items: center; */
         margin: 30px auto;
-        /* max-width: 100%; */
         .car-box {
             width: 100%;
             border-radius: 20px;
@@ -243,7 +385,11 @@ export const FooterContainer = styled.div`
        .footer-none {
             display: none;
        }
-       gap: 50px;
+       /* gap: 50px; */
+       display: flex;
+       justify-content: space-between;
+       align-items: start;
+       padding: 40px 30px;
     }
 `
 
